7212644fd95d006cb39dfe771e1c350e5bf26190
Jet132 <37117573+Jet132@users.noreply.github.com>
2020-07-22 21:08:07 +0200

    implemented discord login

--- server.js
+++ server.js
@@ -1,34 +1,71 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
+require('dotenv').config();
 const express = require("express");
+const fs = require("fs");
+const DiscordOauth2 = require("discord-oauth2");
+const asyncHandler = require('express-async-handler');
+
+function readViewFile(name) {
+  return fs.readFileSync(__dirname + `/views/${name}.html`).toString();
+}
+
+function insertValues(base, values) {
+  for (const [key, value] of Object.entries(values))
+    base = base.replace(`{{${key.toLocaleUpperCase()}}}`, value);
+  return base;
+}
+
+const indexContent = insertValues(readViewFile('index'), {
+  CLIENT_ID: process.env.CLIENT_ID,
+  REDIRECT_URI: encodeURIComponent(process.env.REDIRECT_URI)
+});
+const errorContent = readViewFile('error');
+const resultContent = readViewFile('result');
+
+const oauth = new DiscordOauth2({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  redirectUri: process.env.REDIRECT_URI
+});
+
 const app = express();
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
-});
+app.get("/", asyncHandler(async (req, res) => {
+  if (!req.query.code)
+    return res.send(indexContent);
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
+  let result = undefined;
+  try {
+    result = await oauth.tokenRequest({
+      code: req.query.code,
+      grantType: "authorization_code",
+      scope: ['identify']
+    });
+  } catch{
+    return res.send(errorContent.replace('{{REASON}}', 'The access code seems to be incorrect.'));
+  }
 
-// listen for requests :)
+  let user = undefined;
+  try {
+    user = await oauth.getUser(result.access_token);
+  } catch{
+    return res.send(errorContent.replace('{{REASON}}', 'The server was unable to retrieve the user info from Discord.'));
+  }
+
+  return res.send(insertValues(resultContent, {
+    USER_NAME: user.username,
+    USER_DISC: user.discriminator,
+    USER_ID: user.id,
+    USER_AVATAR: user.avatar
+  }));
+}));
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
+
 const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+  console.log(`Node ${process.env.NODE_ID} listening on port ${process.env.PORT}`);
 });
